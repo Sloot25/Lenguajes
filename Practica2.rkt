4bb0356c8acc2881e7e02c62054b7e19aeb7edf7
Diego Vidal <vidalaguilardiego@ciencias.unam.mx>
2024-02-28 09:13:02 -0600

    Practica2

---
+++ Practica2.rkt
@@ -0,0 +1,41 @@
+#lang plai
+#|
+Practica 2.
+|#
+
+;; Ejercicio 1
+(define (mapea func ls)
+  (error "Sin implementar"))
+
+;; Ejercicio 2
+(define (get-by-index index ls)
+  (error "Sin implementar"))
+
+;; Ejercicio 3
+;; Define el tipo Figura aquí.
+
+;; Ejercicio 4
+(define (area figura)
+  (error "Sin implementar"))
+
+;; Ejercicio 5
+(define (perimetro figura)
+  (error "Sin implementar"))
+
+;; Ejercicio 6
+
+(define-type ArbolDeBusqueda
+  [vacio]
+  [nodo (elem number?) (izq ArbolDeBusqueda?) (der ArbolDeBusqueda?)])
+
+;; Ejercicio 6a
+(define (elimina ar el)
+  (error "Sin implementar"))
+
+;; Ejercicio 6b
+(define (contiene? ar el)
+  (error "Sin implementar"))
+
+;; Ejercicio 6c
+(define (filtrar-arbol ar pred)
+  (error "Sin implementar"))
