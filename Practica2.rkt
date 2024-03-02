8abc4627125b5e08deee021d201ab9a05f4e7eb7
Diego Vidal <vidalaguilardiego@ciencias.unam.mx>
2024-03-02 11:33:13 -0600

    Falta arboles

--- Practica2.rkt
+++ Practica2.rkt
@@ -9,18 +9,31 @@ Practica 2.
 
 ;; Ejercicio 2
 (define (get-by-index index ls)
-  (error "Sin implementar"))
+  (cond
+    [(< index 0) (error "El indice no puede ser menor que 0")]
+    [(and (> index -1) (empty? ls)) (error "El indice es mayor o igual al tamaño de la lista")]
+    [(= index 0) (car ls)]
+    [else (get-by-index (- index 1)(rest ls))]))
 
 ;; Ejercicio 3
 ;; Define el tipo Figura aquí.
+(define-type Figura
+  [cuadrado (lado number?)]
+  [circulo (diametro number?)])
 
 ;; Ejercicio 4
 (define (area figura)
-  (error "Sin implementar"))
+  (cond
+  [(cuadrado? figura) (* (cuadrado-lado figura) (cuadrado-lado figura))]
+  [(circulo? figura) (* (* (/ (circulo-diametro figura) 2) (/ (circulo-diametro figura) 2)) pi)]
+  [else (error "El parametro recibido no es del tipo figura")]))
 
 ;; Ejercicio 5
 (define (perimetro figura)
-  (error "Sin implementar"))
+  (cond
+  [(cuadrado? figura) (* 4 (cuadrado-lado figura))]
+  [(circulo? figura) (* (circulo-diametro figura) pi)]
+  [else (error "El parametro recibido no es del tipo figura")]))
 
 ;; Ejercicio 6
 
