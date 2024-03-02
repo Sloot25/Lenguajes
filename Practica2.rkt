64ba6a5e80e78cdae56f19465b8148130d528f30
Diego Vidal <vidalaguilardiego@ciencias.unam.mx>
2024-03-02 14:48:35 -0600

    No funciona el ultimo inciso

--- Practica2.rkt
+++ Practica2.rkt
@@ -41,14 +41,45 @@ Practica 2.
   [vacio]
   [nodo (elem number?) (izq ArbolDeBusqueda?) (der ArbolDeBusqueda?)])
 
+(define (addElem ar el)
+ (cond
+  [(vacio? ar) (nodo el (vacio) (vacio))]
+  [(nodo? ar) (if (< (nodo-elem ar) el) (nodo (nodo-elem ar) (nodo-izq ar) (addElem (nodo-der ar) el))
+                  (nodo (addElem (nodo-izq ar) el) (nodo-der ar)))]
+  [else (error "El arbol ingresado no es un arbol de busqueda")]))
 ;; Ejercicio 6a
 (define (elimina ar el)
-  (error "Sin implementar"))
+  (cond
+    [(vacio? ar) vacio]
+    [(nodo? ar) (cond
+                   [(= (nodo-elem ar) el) (if (vacio? (nodo-izq ar)) (nodo-der ar) (nodo (buscaMaximo (nodo-izq ar)) (eliminaMaximo (nodo-izq ar)) (nodo-der ar)))]
+                   [(< (nodo-elem ar) el)  (nodo (nodo-elem ar) (nodo-izq ar) (elimina (nodo-der ar) el))]
+                   [else (nodo (nodo-elem ar) (elimina (nodo-izq ar) el) (nodo-der ar))]) ]
+    [else (error "No has pasado un arbol como parametro")]))
+#|La funcion no recibe arboles vacios, es solo una funcion auxiliar |#
+(define (eliminaMaximo ar)
+  (cond
+    [(vacio? (nodo-der ar)) (nodo-izq ar)]
+    [else (nodo (nodo-elem ar) (nodo-izq ar) (eliminaMaximo (nodo-der ar)))]))
+(define (buscaMaximo ar)
+  (cond
+    [(vacio? (nodo-der ar)) (nodo-elem ar)]
+    [else (buscaMaximo (nodo-der ar))]))
 
 ;; Ejercicio 6b
 (define (contiene? ar el)
-  (error "Sin implementar"))
+  (cond
+    [(vacio? ar) #f]
+    [(= (nodo-elem ar) el) #t]
+    [(< (nodo-elem ar) el) (contiene? (nodo-der ar) el)]
+    [(> (nodo-elem ar) el) (contiene? (nodo-izq ar) el)]
+    [else (error "No has ingresado un arbol binario de busqueda")]))
 
 ;; Ejercicio 6c
-(define (filtrar-arbol ar pred)
-  (error "Sin implementar"))
+ (define (filtrar-arbol ar pred)
+  (cond
+    [(vacio? ar) vacio]
+    [(nodo? ar) (if (pred (nodo-elem ar))
+                    (nodo (nodo-elem ar) (filtrar-arbol (nodo-izq ar) pred) (filtrar-arbol (nodo-der ar) pred))
+                    (elimina (nodo (nodo-elem ar) (filtrar-arbol (nodo-izq ar) pred) (filtrar-arbol (nodo-der ar) pred)) (nodo-elem ar)))]
+    [else (error "No has ingresado un arbol correctamente")]))
