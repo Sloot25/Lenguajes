0b0f0542ca0935127d86d0a8a211ff9c64e5d4ff
HugoPaniagua <112269028+CodeDunker2004@users.noreply.github.com>
2024-06-07 11:19:02 -0600

    Update grammars.rkt

--- Practica6/grammars.rkt
+++ Practica6/grammars.rkt
@@ -1,8 +1,10 @@
 #lang plai
 
+;; Definicion del binding 
 (define-type Binding
   [binding (id symbol?) (value RCFSBAE?)])
 
+;; Definicion de los tipos dentro del lenguaje RCFSBAE
 (define-type RCFSBAE
   [num (literal number?)]
   [id (literal symbol?)]
@@ -14,15 +16,18 @@
   [iF (test-expr RCFSBAE?) (then-expr RCFSBAE?) (else-expr RCFSBAE?)]
   [rec (bindings (listof Binding?)) (body RCFSBAE?)])
 
+;; Definicion de un valor en nuestro lenguaje
 (define-type Value
   [numV (literal number?)]
   [boolV (literal boolean?)]
   [strV (literal string?)]
   [closureV (params (listof symbol?)) (body RCFSBAE?) (env Env?)])
 
+;; Definicion de las cajitas que vamos a usar para cuando trabajemos con memoria 
 (define (boxed-Value? v)
   (and (box? v) (Value? (unbox v))))
 
+;; Definicion de ambiente
 (define-type Env
   [mt-env]
   [cons-env (id symbol?) (value Value?) (rest Env?)]
